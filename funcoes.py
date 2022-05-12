9111a34768df4cad1c9a64795e69c9f7de34097b
luisaabibi <104869854+luisaabibi@users.noreply.github.com>
2022-05-12 19:49:44 -0300

    Create funcoes.py

---
+++ funcoes.py
@@ -0,0 +1,19 @@
+import math
+import random
+
+def haversine (r, p1, l1, p2, l2):
+    p1_rad= math.radians(p1)
+    p2_rad= math.radians(p2)
+    l1_rad= math.radians(l1)
+    l2_rad= math.radians(l2)
+
+    
+    d1= (math.sin((p2_rad-p1_rad)/2))**2
+    d2= math.cos(p1_rad)
+    d3= math.cos(p2_rad)
+    d4= (math.sin((l2_rad- l1_rad)/2))**2
+    dt= d1 + (d2*d3*d4)
+
+    d= 2 * r * math.asin(dt**(1/2))
+
+    return d
