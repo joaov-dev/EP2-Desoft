0db2bfe06d75cb392e3b5319618c4e5f4e02370c
luisaabibi <104869854+luisaabibi@users.noreply.github.com>
2022-05-14 00:00:56 -0300

    Update jogo.py

--- jogo.py
+++ jogo.py
@@ -8,6 +8,7 @@ EARTH_RADIUS = 6371
 tentativas = 20
 listaTentivas = []
 listaChutes = []
+mostraChutes= []
 continuaJogo = True
 
 coresUsadas = []
@@ -16,29 +17,17 @@ paisSorteado = sorteia_pais(dic_normalizado)
 entraPais = input('Escolha um pais para comecar: ')
 
 
+while continuaJogo == True:
+    while entraPais != paisSorteado:
 
-if entraPais in dic_normalizado.keys():
+        if:
+            if entraPais in dic_normalizado.keys():
                 if entraPais not in listaChutes:
                     if tentativas > 0:
                         dist = haversine(EARTH_RADIUS, dic_normalizado[paisSorteado]['geo']['latitude'], dic_normalizado[paisSorteado]['geo']['longitude'],dic_normalizado[entraPais]['geo']['latitude'], dic_normalizado[entraPais]['geo']['longitude'])
                         adiciona_em_ordem(entraPais, dist, listaTentivas)
                         listaChutes.append(entraPais)
 
-                        print('Errou, o pais secreto esta a: {0}'.format(dist))
+                        mostraChutes = adiciona_em_ordem(entraPais, dist, mostraChutes)
 
-                        entraPais = input('Escolha mais um pais: ')
-                        
-                    else:
-                        print('Voce perdeu! ')
-                    
-                    tentativas -= 1
-                    print('Voce tem {} tentativas restantes!'.format(tentativas))
-
-                else:
-                    print('Voce ja chutou esse pais!')
-
-                    entraPais = input('Escolha outro pais: ')
-            else:
-                print('Este pais nao existe, ou nao esta em nosso banco de dados!')
-                
-                entraPais = input('Escolha mais um pais: ')
+                        print('\nErrou, o pais secreto esta a: {:.2f}'.format(dist))
